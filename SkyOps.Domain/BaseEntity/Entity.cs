353494b3e667982fc3857226895fa6be3fd40940
Ahmed Ehab <ahmedesrm@gmail.com>
2025-03-04 13:31:44 +0300

    Created base entity

---
+++ SkyOps.Domain/BaseEntity/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyOps.Domain.BaseEntity
+{
+    public abstract class Entity
+    {
+        public virtual int Id { get; set; }
+
+    }
+}
