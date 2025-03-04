64bf0d1f85337da6661bda6f7f7c6dddf0070dea
Ahmed Ehab <ahmedesrm@gmail.com>
2025-03-04 13:31:55 +0300

    Initial entities

---
+++ SkyOps.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using SkyOps.Domain.BaseEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyOps.Domain.Entities
+{
+    public class User : Entity
+    {
+    }
+}
